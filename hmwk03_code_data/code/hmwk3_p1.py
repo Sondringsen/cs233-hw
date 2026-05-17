aa9ab7bb374bd5c0c382ff9a2444eb850ddd22a4
Sondre Rogde <sondre.rogde@gmail.com>
2026-05-17 14:03:26 -0700

    hw3 p1a and p1b

--- hmwk03_code_data/code/hmwk3_p1.py
+++ hmwk03_code_data/code/hmwk3_p1.py
@@ -9,9 +9,11 @@ import skimage.transform
 import skimage.feature
 import os
 import matplotlib.pyplot as plt
+from pathlib import Path
 
 from mrf import mrf
 
+PLOT_SAVE_PATH = Path("hmwk03_code_data/plots/")
 
 
 # ---Visualization Utilities--- #
@@ -24,7 +26,7 @@ def mat_show(matrix):
 	"""
 	plt.matshow(matrix)
 	plt.colorbar()
-	plt.show()
+	# plt.show()
 
 
 def save_hog_image(hog_image, title, output_path):
@@ -45,7 +47,7 @@ def save_hog_image(hog_image, title, output_path):
 # ----------------------------- #
 
 # directory of rendered views of each shape
-rendering_dir = '../data_p1/100chairs_rendering'
+rendering_dir = 'hmwk03_code_data/data_p1/100chairs_rendering'
 # number of chairs in our dataset
 num_shapes = 100
 # number of rendered views for each shape
@@ -74,6 +76,12 @@ def hog_extraction(image):
 	# You may use skimage.transform.resize and skimage.feature.hog
 	# https://scikit-image.org/docs/stable/auto_examples/features_detection/plot_hog.html
 	# ----------------------------- #
+	resized_image = skimage.transform.resize(image, (120, 120), anti_aliasing=True)
+	fd, hog_image = skimage.feature.hog(
+		resized_image,
+		visualize=True,
+	)
+	return fd, hog_image
 
 
 # ------ Your code here ------- #
@@ -82,6 +90,26 @@ def hog_extraction(image):
 #       by computing the HoG feature of each view
 # You may use skimage.io.imread for loading images
 # ----------------------------- #
+def concat_hog(obj: int = 1) -> np.ndarray:
+	assert obj >= 1 and obj <= 100, "Object must be between 1 and 100"
+	hog_features = []
+
+	for view in range(16):
+		image = skimage.io.imread(rendering_dir + f"/00{obj}_{view}.png", as_gray=True)
+		fd, _ = hog_extraction(image)
+		hog_features.append(fd)
+
+	hog_features = np.array(hog_features)
+	return hog_features
+
+# Visualization
+def visualize_p1a():
+	for obj in range(1, 4):
+		for view in range(3):
+			image = skimage.io.imread(rendering_dir + f"/00{obj}_{view}.png", as_gray=True)
+			_, hog_image = hog_extraction(image)
+			save_hog_image(hog_image, f"Hog image object = {obj}, view = {view}", PLOT_SAVE_PATH / f"hog_image_00{obj}_{view}.png")
+
 
 
 # ----------------------------- #
@@ -106,6 +134,48 @@ def pairwise_dissimilarity(feat1, feat2):
 	"""
 	# ------ Your code here ------- #
 	# ----------------------------- #
+	V = 16
+	D = np.zeros((V, V), float)
+	for theta1 in range(V):
+		for theta2 in range(V):
+			rotation1 = np.roll(feat1, theta1, axis=0)
+			rotation2 = np.roll(feat2, theta2, axis=0)
+			D[theta1, theta2] = np.linalg.norm(rotation1 - rotation2)
+
+	return D
+
+# Visualization
+def visualization_p1b():
+	feat1 = concat_hog(1)
+	feat2 = concat_hog(2)
+	D = pairwise_dissimilarity(feat1, feat2)
+	mat_show(D)
+	plt.savefig(PLOT_SAVE_PATH / "d_matrix.png")
+	
+	# Finding best and worst alignment
+	print(np.argmax(D))
+	best_flat_idx = np.argmin(D)
+	b_i, b_j = np.unravel_index(best_flat_idx, D.shape)
+	print(b_i, b_j)
+
+	worst_flat_idx = np.argmax(D)
+	w_i, w_j = np.unravel_index(worst_flat_idx, D.shape)
+	print(w_i, w_j)
+
+	fig, axs = plt.subplots(2, 2)
+
+	image = skimage.io.imread(rendering_dir + f"/00{1}_{b_i}.png")
+	axs[0][0].imshow(image)
+	image = skimage.io.imread(rendering_dir + f"/00{2}_{b_j}.png")
+	axs[0][1].imshow(image)
+
+	image = skimage.io.imread(rendering_dir + f"/00{1}_{w_i}.png")
+	axs[1][0].imshow(image)
+	image = skimage.io.imread(rendering_dir + f"/00{2}_{w_j}.png")
+	axs[1][1].imshow(image)
+	fig.suptitle("Best alignment top, worst alignment bottom")
+	plt.tight_layout()
+	plt.savefig(PLOT_SAVE_PATH / "worst_best_alignment.png")
 
 
 
@@ -120,3 +190,20 @@ def pairwise_dissimilarity(feat1, feat2):
 # TODO: build unary vector U
 # TODO: call MRF solver from mrf.py to jointly align shapes
 # ----------------------------- #
+
+
+
+
+
+def main():
+	pass
+	# 1a
+	# hog_features = concat_hog()
+	# print(hog_features)
+	# visualize_p1a()
+
+	# 1b
+	# visualization_p1b()
+
+if __name__ == "__main__":
+	main()
